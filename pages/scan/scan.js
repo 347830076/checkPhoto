30b0e018d340b80e3739e48571c6c70966a642e7
cylyiou <347830076@qq.com>
2018-08-18 18:13:45 +0800

    .

---
+++ pages/scan/scan.js
@@ -0,0 +1,99 @@
+// pages/scan/scan.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.scanFun();
+  },
+  scanFun:function(){
+    // 允许从相机和相册扫码
+    let that = this;
+    wx.scanCode({
+      success: (res) => {
+        console.log(res);
+        that.setData({
+          result: res.result,
+          scanType: res.scanType == "QR_CODE" ? "二维码" : "条型码",
+          charSet: res.charSet,
+        })
+
+      }
+    })
+  },
+  copy:function(e){
+    console.log(e)
+    wx.setClipboardData({
+      data: e.target.dataset.result,
+      success: function (res) {
+        wx.showToast({
+          title: '复制成功',
+          icon: 'none',
+          duration: 2000
+        })
+      },fail:function(){
+        wx.showToast({
+          title: '复制失败，请重新复制',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
